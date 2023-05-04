4bcbe8d6fc22ec93e76dda22f9525f3dd69eef8c
Tyffhabwe <tyffhabwe@LAPTOP-G1N9N257>
2023-05-04 21:44:59 +1200

    GET and PUT are mostly working now I think.

--- files/server.c
+++ files/server.c
@@ -20,19 +20,31 @@
 
 
 /** A store of messages that are useful */
+char* test = "YOU ARE OUT OF THE WRITING PHASE BROTHER";
 char* start_message = "HELLO\n";
 char* server_ok_message = "SERVER 200 OK\n\n";
 char* server_error_message = "SERVER 404 Not Found\n";
 char* server_505_message = "SERVER 501 Put Error\n";
 char* server_500_message = "SERVER 500 Get Error\n";
+char* server_501_message = "SERVER 501 Put Error\n";
+char* server_502_message = "SERVER 502 Command Error\n";
 char* newline_at_end = "\n\n";
 char* singular_newline_char = "\n";
 
+/** Helper method that throws an error and exits when stuff go wrong*/
 void error(const char * msg){
     printf("Error : %s.\n", msg);
     exit(-1);
-} 
+}
 
+/** Helped method that takes content from the client and copies it to buffer */
+void read_user_content_to_buffer(char buffer[], int client_fd, int buflen) {
+    memset(buffer, 0, buflen);
+    int r = read(client_fd, buffer, buflen);
+    if(r < 0) {error("Error reading from client socket");}
+}
+
+/** Helper method to write whatever is in the buffer to the user */
 void write_to_socket(int client_fd, char buffer[], int buflen) {
     int s = write(client_fd, buffer, buflen);
     if(s<0) {
@@ -41,14 +53,15 @@ void write_to_socket(int client_fd, char buffer[], int buflen) {
     printf("Successful write\n");
 }
 
+/** Helper method to write a specific message to the user */
 void write_to_user(char buffer[], char* message, int message_length, int client_fd, int buflen) {
     memset(buffer, 0, buflen);
     strncpy(buffer, message, message_length);
     write_to_socket(client_fd, buffer, buflen);
 }
 
+/** Method that handles getting the files and printing stuff back for a get response */
 void handle_get_response(char buffer[], int client_fd, int buflen) {
-    
     int server_ok_message_len = strlen(server_ok_message); 
     int server_error_message_len = strlen(server_error_message);
     int server_505_len = strlen(server_505_message);
@@ -59,9 +72,7 @@ void handle_get_response(char buffer[], int client_fd, int buflen) {
     FILE *in;
     char command[3];
     char filename[20];
-
     memset(filename, '\0', 20);
-
     sscanf(buffer, "%s %s", command, filename);
     
     if(filename[0] == '\0') {
@@ -91,10 +102,55 @@ void handle_get_response(char buffer[], int client_fd, int buflen) {
     fclose(in);
 }
 
+/** Method that handles file editing work for a put response */
 void handle_put_request(char buffer[], int client_fd, int buflen) {
-    /*
-    * THEN FINISH THIS METHOD WITH GREAT INSIPIRATION FROM ABOVE
-    */
+    int taking_user_input = 1; //Boolean on whether to keep taking input
+    int on_alert = 0; //Boolean that can either be 0 or 1.
+    int server_error_message_len = strlen(server_error_message);
+
+    FILE *in;
+    char command[3];
+    char filename[20];
+    memset(filename, '\0', 20);
+    sscanf(buffer, "%s %s", command, filename);
+
+    in = fopen(filename, "w");
+
+    if(in == NULL) {
+        write_to_user(buffer, server_501_message, strlen(server_501_message), client_fd, buflen);
+        return;
+    }
+
+    read_user_content_to_buffer(buffer, client_fd, buflen);
+    while(taking_user_input == 1) {
+        //Being on alert means the last buffer was a '\n'
+        if(on_alert == 1) {
+            //Means we have found a second straight '\n' and so stop taking user input
+            if(strncmp(buffer, "\n", 1) == 0) {
+                taking_user_input = 0;
+            }
+            //Means we found an '\n' in previous buffer but have found something else so no longer on alert
+            else{
+                on_alert = 0;
+            }
+        }
+        //We are not currently on alert
+        else {
+            //We have found a first '\n' and so need to be 'on alert' for the second '\n'
+            if(strncmp(buffer, "\n", 1) == 0) {
+                on_alert = 1;
+            }
+        }
+
+        fputs(buffer, in);
+
+        printf("What is in the buffer: %s\n", buffer);
+        read_user_content_to_buffer(buffer, client_fd, buflen);
+    }
+
+    write_to_user(buffer, test, strlen(test), client_fd, buflen);
+    fclose(in);
+
 }
 /**
  * The main function should be able to accept a command-line argument
@@ -142,6 +198,7 @@ int main(int argc, char *argv[])
     }
 
     while(1) {
+
         //Time to listen for connections
         if(listen(fd, SOMAXCONN) < 0) {
             error("Listening error");
@@ -160,18 +217,14 @@ int main(int argc, char *argv[])
         char buffer[buflen];
         
         write_to_user(buffer, start_message, strlen(start_message), client_fd, buflen);
-        
-        memset(buffer, 0, buflen);
-        int r = read(client_fd, buffer, buflen);
-
+        read_user_content_to_buffer(buffer, client_fd, buflen);
+    
         //Keep running until I get 'bye' or BYE in the first 3 letters from user
         /**
          * MIGHT WANNA CHANGE THIS TO A .CONTAINS() METHOD IF U CAN FIND IT SOMEWHERE
         */
         while(strncmp(buffer, "bye", 3) != 0 && strncmp(buffer, "BYE", 3) != 0) {
-            if(r < 0) {
-                error("Error reading from client socket");
-            }
+            
             if(strncmp(buffer, "get", 3) == 0 || strncmp(buffer, "GET", 3) == 0) {
                 handle_get_response(buffer, client_fd, buflen);
             }
@@ -179,13 +232,10 @@ int main(int argc, char *argv[])
                 handle_put_request(buffer, client_fd, buflen);
             }
             else {
-                printf("Did not recognise command: %s\n", buffer);
+                write_to_user(buffer, server_502_message, strlen(server_502_message), client_fd, buflen);
             }
-            memset(buffer, 0, buflen);
-            r = read(client_fd, buffer, buflen);
+            read_user_content_to_buffer(buffer, client_fd, buflen);
         }
-        
-        //END THE CONNECTION
         close(client_fd);
         printf("Closed connection!\n");
     }
